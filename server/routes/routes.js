365e269e536e8ad687c0d6dae1b6cc793f5eb497
JPZ13 <j.p.zivalich@gmail.com>
2017-03-17 00:10:40 -0700

    edit http code

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const path = require( 'path' );
 
 router.get( '/', ( req, res ) => {
-  res.sendFile('index.html', { root: path.join( __dirname, '../../client' )});
+  res.status( 200 ).sendFile('index.html', { root: path.join( __dirname, '../../client' )});
 });
 
 module.exports = router;
